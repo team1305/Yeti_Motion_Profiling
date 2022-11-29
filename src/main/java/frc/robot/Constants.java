4a3f53411466dc493a1852a08335e7545c77327d
Programming Laptop <programming@team1305.org>
2022-11-28 21:50:34 -0500

    FIrst commit

---
+++ src/main/java/frc/robot/Constants.java
@@ -0,0 +1,65 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot;
+
+import com.ctre.phoenix.motorcontrol.StatorCurrentLimitConfiguration;
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonFX;
+
+import edu.wpi.first.math.kinematics.DifferentialDriveKinematics;
+
+/**
+ * The Constants class provides a convenient place for teams to hold robot-wide numerical or boolean
+ * constants. This class should not be used for any other purpose. All constants should be declared
+ * globally (i.e. public static). Do not put anything functional in this class.
+ *
+ * <p>It is advised to statically import this class (or one of its inner classes) wherever the
+ * constants are needed, to reduce verbosity.
+ */
+public final class Constants {
+
+    
+    //Motor ID for the drive base
+	public static WPI_TalonFX mtDriveRight2 = new WPI_TalonFX(11);
+	public static WPI_TalonFX mtDriveRight1 = new WPI_TalonFX(12);
+	public static WPI_TalonFX mtDriveLeft1 = new WPI_TalonFX(13);
+	public static WPI_TalonFX mtDriveLeft2 = new WPI_TalonFX(14);
+    public final static StatorCurrentLimitConfiguration currentLimitConfig = new StatorCurrentLimitConfiguration(true, 40, 39.95, 1);
+	public final static StatorCurrentLimitConfiguration currentLimitConfig30 = new StatorCurrentLimitConfiguration(true, 30, 29.95, 1);
+
+	//public static final int[] kLeftEncoderPorts = new int[] {13, 14};
+	//public static final int[] kRightEncoderPorts = new int[] {11, 12};
+	//public static final boolean kLeftEncoderReversed = false;
+	//public static final boolean kRightEncoderReversed = false;
+
+	public static final int kEncoderCPR = 2048;
+	public static final double kWheelDiameterMeters = 0.1524;// REMEBER TO PUT MEASUREMENTS IN METERS
+	public static final double kGearBoxReduction = 10.71;
+	public static final double kEncoderDistancePerPulse = (kWheelDiameterMeters * Math.PI) / (double) kEncoderCPR / kGearBoxReduction;
+	//public static final double kEncoderDistancePerPulse = 512;
+
+    public static final double ksVolts = 0.61758;
+    public static final double kvVoltSecondsPerMeter = 1.1824;
+    public static final double kaVoltSecondsSquaredPerMeter = 0.20101;
+
+
+    public static final double kPDriveVel = 1.6677;
+	
+
+	public static final double kTrackwidthMeters = 0.453;//CHANGE THIS TO BE ACTUAL WIDTH
+    public static final DifferentialDriveKinematics kDriveKinematics =
+        new DifferentialDriveKinematics(kTrackwidthMeters);
+
+		
+	public static final double kMaxSpeedMetersPerSecond = 1;//set low test
+	public static final double kMaxAccelerationMetersPerSecondSquared = 1;//set low to test
+
+	    // Reasonable baseline values for a RAMSETE follower in units of meters and seconds
+		public static final double kRamseteB = 2;
+		public static final double kRamseteZeta = 0.7;
+
+
+
+
+}
---
+++ src/main/java/frc/robot/subsystems/Subsystem_Drivebase_Motion.java
@@ -0,0 +1,239 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems;
+
+
+import com.ctre.phoenix.motorcontrol.NeutralMode;
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonFX;
+
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.kinematics.DifferentialDriveOdometry;
+import edu.wpi.first.math.kinematics.DifferentialDriveWheelSpeeds;
+import edu.wpi.first.math.util.Units;
+//import edu.wpi.first.wpilibj.Encoder;
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+//import edu.wpi.first.wpilibj.examples.ramsetecommand.Constants;
+import edu.wpi.first.wpilibj.interfaces.Gyro;
+import edu.wpi.first.wpilibj.motorcontrol.MotorControllerGroup;
+//import edu.wpi.first.wpilibj.motorcontrol.PWMSparkMax;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants;
+//import frc.robot.RobotContainer;
+
+import com.kauailabs.navx.frc.AHRS;
+import edu.wpi.first.wpilibj.SPI;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+
+public class Subsystem_Drivebase_Motion extends SubsystemBase {
+  AHRS navx;
+  //private boolean bteleop;
+
+    // grabs drive motor information from RobotMap
+    private final static WPI_TalonFX mtLeft1 = Constants.mtDriveLeft1;
+    private final static WPI_TalonFX mtLeft2 = Constants.mtDriveLeft2;
+    private final static WPI_TalonFX mtRight1 = Constants.mtDriveRight1;
+    private final static WPI_TalonFX mtRight2 = Constants.mtDriveRight2;
+
+        // creates motor controller groups for left and right motors
+        final  MotorControllerGroup m_leftMotors = new MotorControllerGroup(mtLeft1, mtLeft2);
+        final  MotorControllerGroup m_rightMotors = new MotorControllerGroup(mtRight1, mtRight2);
+    
+
+
+  // The robot's drive
+  private final DifferentialDrive m_drive = new DifferentialDrive(m_leftMotors, m_rightMotors);
+
+  // The gyro sensor
+  private final Gyro m_gyro = new AHRS(SPI.Port.kMXP);
+ 
+
+
+  // Odometry class for tracking robot pose
+  private final DifferentialDriveOdometry m_odometry;
+
+  /** Creates a new DriveSubsystem. */
+  public Subsystem_Drivebase_Motion() {
+    // We need to invert one side of the drivetrain so that positive voltages
+    // result in both sides moving forward. Depending on how your robot's
+    // gearbox is constructed, you might have to invert the left side instead.
+    m_gyro.reset();
+
+
+    mtLeft1.configFactoryDefault();
+    mtLeft2.configFactoryDefault();
+    mtRight1.configFactoryDefault();
+    mtRight2.configFactoryDefault();
+
+    mtLeft1.setNeutralMode(NeutralMode.Brake);
+    mtLeft2.setNeutralMode(NeutralMode.Brake);
+    mtRight1.setNeutralMode(NeutralMode.Brake);
+    mtRight2.setNeutralMode(NeutralMode.Brake);
+
+    m_leftMotors.setInverted(false); // this makes the robot spin really fast
+    m_rightMotors.setInverted(true);
+
+    resetEncoders();
+    m_odometry = new DifferentialDriveOdometry(m_gyro.getRotation2d());
+  }
+
+  @Override
+  public void periodic() {
+    // Update the odometry in the periodic block
+    //if (bteleop = false) {
+       m_odometry.update(
+        m_gyro.getRotation2d(), getLeftEncoderDistance(), getRightEncoderDistance());
+       SmartDashboard.putNumber("Right Encoder (Ticks)", mtRight1.getSelectedSensorPosition());
+       SmartDashboard.putNumber("Left Encoder (Ticks)", mtLeft1.getSelectedSensorPosition());
+       SmartDashboard.putNumber("Right Encoder Distance (Meteres)", getRightEncoderDistance());
+       SmartDashboard.putNumber("Left Encoder Distance (Meteres)", getLeftEncoderDistance());
+       SmartDashboard.putNumber("Right Encoder Speed", getRightEncoderSpeed());
+       SmartDashboard.putNumber("Left Encoder Speed", getLeftEncoderSpeed());
+       SmartDashboard.putNumber("GYRO", m_gyro.getAngle());
+    //} 
+  }
+
+  /*
+  public static double getEncRightSide() {
+    return mtRight1.getSelectedSensorPosition();
+  }
+
+   public void set_teleop(boolean bmode) {
+     bteleop = bmode;
+   }
+   
+
+  public static double getEncLeftSide() {
+    return mtLeft1.getSelectedSensorPosition();
+  }
+  */
+
+  private double nativeUnitsToDistanceMeters(double sensorCounts){
+    double kCountsPerRev = 2048;
+    double kGearRatio = (50/14)*(48/16);//10.71428571428571;
+    double kWheelRadiusInches = 6/2;
+		double motorRotations = (double)sensorCounts / kCountsPerRev;
+		double wheelRotations = motorRotations / kGearRatio;
+		double positionMeters = wheelRotations * (2 * Math.PI * Units.inchesToMeters(kWheelRadiusInches));
+		return positionMeters;
+	}
+
+  private double nativeUnitsToVelocityMetersPerSecond(double sensorCountsPer100ms){
+    double k100msPerSecond = 10;
+    double velocityMetersPerSecond = nativeUnitsToDistanceMeters(sensorCountsPer100ms) * k100msPerSecond;
+		return velocityMetersPerSecond;
+	}
+  public double getLeftEncoderDistance() {
+    return nativeUnitsToDistanceMeters(mtLeft1.getSelectedSensorPosition());
+  }
+
+  public double getRightEncoderDistance() {
+    return nativeUnitsToDistanceMeters(mtRight1.getSelectedSensorPosition());
+  }
+
+  public double getLeftEncoderSpeed() {
+    return nativeUnitsToVelocityMetersPerSecond(mtLeft1.getSelectedSensorVelocity());
+  }
+
+  public double getRightEncoderSpeed() {
+    return nativeUnitsToVelocityMetersPerSecond(mtRight1.getSelectedSensorVelocity());
+  }
+
+  /**
+   * Returns the currently-estimated pose of the robot.
+   *
+   * @return The pose.
+   */
+  public Pose2d getPose() {
+    return m_odometry.getPoseMeters();
+  }
+
+  /**
+   * Returns the current wheel speeds of the robot.
+   *
+   * @return The current wheel speeds.
+   */
+  public DifferentialDriveWheelSpeeds getWheelSpeeds() {
+    return new DifferentialDriveWheelSpeeds(getLeftEncoderSpeed(), getRightEncoderSpeed());
+  }
+
+  /**
+   * Resets the odometry to the specified pose.
+   *
+   * @param pose The pose to which to set the odometry.
+   */
+  public void resetOdometry(Pose2d pose) {
+    resetEncoders();
+    m_odometry.resetPosition(pose, m_gyro.getRotation2d());
+  }
+
+  /**
+   * Drives the robot using arcade controls.
+   *
+   * @param fwd the commanded forward movement
+   * @param rot the commanded rotation
+   */
+  public void arcadeDrive(double fwd, double rot) {
+    m_drive.arcadeDrive(fwd, rot);
+  }
+
+  /**
+   * Controls the left and right sides of the drive directly with voltages.
+   *
+   * @param leftVolts the commanded left output
+   * @param rightVolts the commanded right output
+   */
+  public void tankDriveVolts(double leftVolts, double rightVolts) {
+    m_leftMotors.setVoltage(leftVolts);
+    m_rightMotors.setVoltage(rightVolts);
+    m_drive.feed();
+  }
+
+  /** Resets the drive encoders to currently read a position of 0. */
+  public void resetEncoders() {
+    mtLeft1.setSelectedSensorPosition(0);
+    mtRight1.setSelectedSensorPosition(0);
+  }
+
+  /**
+   * Gets the average distance of the two encoders.
+   *
+   * @return the average of the two encoder readings
+   */
+  public double getAverageEncoderDistance() {
+    return (getLeftEncoderDistance() + getRightEncoderDistance()) / 2.0;
+  }
+
+  /**
+   * Sets the max output of the drive. Useful for scaling the drive to drive more slowly.
+   *
+   * @param maxOutput the maximum output to which the drive will be constrained
+   */
+  public void setMaxOutput(double maxOutput) {
+    m_drive.setMaxOutput(maxOutput);
+  }
+
+  /** Zeroes the heading of the robot. */
+  public void zeroHeading() {
+    m_gyro.reset();
+  }
+
+  /**
+   * Returns the heading of the robot.
+   *
+   * @return the robot's heading in degrees, from -180 to 180
+   */
+  public double getHeading() {
+    return m_gyro.getRotation2d().getDegrees();
+  }
+
+  /**
+   * Returns the turn rate of the robot.
+   *
+   * @return The turn rate of the robot, in degrees per second
+   */
+  public double getTurnRate() {
+    return -m_gyro.getRate();
+  }
+}
